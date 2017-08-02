aad6e6a607fe69b90396534d28480b861c0f13c9
Chase Allen <callen@smarterhq.com>
2017-08-01 23:28:05 -0400

    optimize and comment out

--- fetch_users.js
+++ fetch_users.js
@@ -1,40 +1,43 @@
 let global_usersArr = [],
-    global_usersArrHTML = [];
+    global_usersArrHTML = [],
+    global_l,
+    global_ld;
 
 fetch('https://raw.githubusercontent.com/SpendBridge/exercises/master/data/users.json')
     .then(r => r.json())
     .then(j => {
         addDataString(j);
     })
-    .catch()
-    .catch();
+    .catch(err => console.error(err))
+    .catch(err => console.error(err));
 
-function addDataString(usersArr) {
-    const l = usersArr.length;
+function addDataString(j) {
+    /* j = the fetched user data array */
+    global_l = j.length;
+    global_ld = Math.floor(global_l / 10);
+
     let i = 0;
-
-    while (i < l) {
-        usersArr[i].dataString = usersArr[i].fullName + usersArr[i].email + usersArr[i].accountName + usersArr[i].accountNumber;
-
+    while (i < global_l) {
+        j[i].dataString = j[i].fullName + j[i].email + j[i].accountName + j[i].accountNumber;
         i++;
     }
 
-    global_usersArr = usersArr;
+    /* set global array of users */
+    global_usersArr = j;
 
-    generateHTML(usersArr);
+    /* create array of users in html */
+    generateHTML(j);
 
     /* execution flows to render_users.js */
     renderAllUsers();
 }
 
 /* build the html version of the global users array, and the global users html string */
-function generateHTML(usersArr) {
-    const l = usersArr.length;
+function generateHTML(j) {
     let i = 0;
-
-    while (i < l) {
-        global_usersArrHTML[i] = `<li class="u" data-d="${usersArr[i].dataString}">${usersArr[i].fullName}, <a onclick="eMsg('${usersArr[i].email}')">${usersArr[i].email}</a>, ${usersArr[i].accountName}, <a onclick="aMsg('${usersArr[i].accountName}')">${usersArr[i].accountNumber}</a></li>`;
-
+    while (i < global_l) {
+        // global_usersArrHTML[i] = `<li class="u" data-d="${usersArr[i].dataString}">${usersArr[i].fullName}, <a onclick="eMsg('${usersArr[i].email}')">${usersArr[i].email}</a>, ${usersArr[i].accountName}, <a onclick="aMsg('${usersArr[i].accountName}')">${usersArr[i].accountNumber}</a></li>`;
+        global_usersArrHTML[i] = `<li>${j[i].fullName}, <a onclick="eMsg('${j[i].email}')">${j[i].email}</a>, ${j[i].accountName}, <a onclick="aMsg('${j[i].accountName}')">${j[i].accountNumber}</a></li>`;
         i++;
     }
 }
--- render_users.js
+++ render_users.js
@@ -3,121 +3,164 @@
 
 global_usersArr;
 global_usersArrHTML;
+global_l;
+global_ld;
 */
 
 /* dom vars */
 const filterInp = document.getElementsByClassName('filter-inp')[0],
-    filterBtn = document.getElementsByClassName('filter-btn')[0],
+    // filterBtn = document.getElementsByClassName('filter-btn')[0],
     ulRender = document.getElementsByClassName('users-render')[0],
     infoBar = document.getElementsByClassName('info-bar')[0],
     infoBarMessage = document.querySelector('.message');
 
-filterInp.disabled = true;
-filterBtn.disabled = true;
+function enableFilterUI() {
+    filterInp.disabled = false;
+    // filterBtn.disabled = false;
+}
+
+function disableFilterUI() {
+    filterInp.disabled = true;
+    // filterBtn.disabled = true;
+}
+disableFilterUI();
 
 /* will hold all user dom nodes once they're rendered */
-let usersNodes;
+// let usersNodes;
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 /* functions to control messaging, especially when clicking on emails/accounts */
 
 infoBar.message = msg => {
+    infoBar.style.zIndex = 1;
     infoBar.style.opacity = 1;
-    infoBarMessage.style.transition = 'opacity .01s linear';
-    infoBarMessage.style.opacity = 1;
+    // infoBarMessage.style.transition = 'opacity .01s linear';
+    // infoBarMessage.style.opacity = 1;
     infoBarMessage.textContent = msg;
 }
 
-infoBar.clear = timeout => {
+infoBar.clear = (timeout, fadeTime) => {
     setTimeout(() => {
-        const fadeTime = 250;
-
-        infoBarMessage.style.transition = `opacity ${fadeTime}ms linear`;
-        infoBarMessage.style.opacity = 0;
+        // infoBarMessage.style.transition = `opacity ${fadeTime}ms linear`;
+        // infoBarMessage.style.opacity = 0;
         setTimeout(() => {
             // infoBar.message(null);
+            infoBar.style.zIndex = -1;
             infoBar.style.opacity = 0;
-
-            /* enable input (and button) for filtering */
-            if (filterInp.disabled) {
-                filterInp.disabled = false;
-                filterBtn.disabled = false;
-            }
-        }, fadeTime);
+        }, fadeTime || 0);
     }, timeout || 0);
 };
 
 function eMsg(email) {
     infoBar.message(`You clicked email ${email}`);
-    infoBar.clear(1250);
+    infoBar.clear(500, 500);
 }
 
 function aMsg(acctName) {
     infoBar.message(`You clicked account name ${acctName}`);
-    infoBar.clear(1250);
+    infoBar.clear(500, 500);
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 /* function to render all users in html and get nodes */
 
+let loadedOnce = false;
+
 function renderAllUsers() {
-    /* length divided */
-    let ld = Math.floor(global_usersArrHTML.length / 5);   /* <-- higher the number, faster the initial load */
+    disableFilterUI();
+
+    infoBar.message('Loading...');
 
     /* load a chunk, then the rest */
-    ulRender.innerHTML = global_usersArrHTML.slice(0, ld).join('');
-
-    infoBar.message('Users loaded!');
-    infoBar.clear(250);
+    ulRender.innerHTML = global_usersArrHTML.slice(0, global_ld).join('');
 
     /* large dump to dom; async/setTimeout seems to increase speed of renders */
     setTimeout(() => {
-        ulRender.innerHTML += global_usersArrHTML.slice(ld).join('');
+        ulRender.innerHTML += global_usersArrHTML.slice(global_ld).join('');
+
+        /* only display 'Users loaded!' first time */
+        if (!loadedOnce) {
+            infoBar.message('Users loaded!');
+            infoBar.clear(0, 250);
+
+            loadedOnce = true;
+        } else {
+            infoBar.clear();
+        }
+
+        enableFilterUI();
+
+        filterInp.focus();
 
         /* grab all the users in the html (nodes) */
-        usersNodes = document.getElementsByClassName('u');
+        // usersNodes = document.getElementsByClassName('u');
     });
 }
 
 /*--------------------------------------------------------------------------------------------------------------------*/
 
 filterInp.addEventListener('keyup', filterUsers);
-
-/* TODO: could try the same tactic as above... build a string, drop a chunk, drop the rest??? */
+// filterBtn.addEventListener('click', filterUsers);
 
 function filterUsers() {
-    try {
-        const l = usersNodes.length;
-        let patterns = this.value.split(' '),
-            pattern,
-            i = 0;
-
-        if (patterns.length === 1) {
-            pattern = RegExp(patterns[0], 'i');
-
-            while (i < l) {
-                usersNodes[i].style.display = pattern.test(usersNodes[i].dataset.d) ? 'block' : 'none';
-                i++;
-            }
-
-            infoBar.clear();
-        } else {
-            patterns = patterns.map(pat => RegExp(pat, 'i'));
-
-            let allPatsMatch;
-
-            while (i < l) {
-                allPatsMatch = true;
-                patterns.forEach(pat => {
-                    if (!pat.test(usersNodes[i].dataset.d)) allPatsMatch = false;
-                });
-                usersNodes[i].style.display = allPatsMatch ? 'block' : 'none';
-                i++;
-            }
+    if (filterInp.value === '') {
+        renderAllUsers();
+    } else {
+        if (filterInp.value.length === 1) {
+            infoBar.message('Loading...');
         }
-    } catch (err) {
-        console.error(err);
+        try {
+            let patterns = filterInp.value.split(' '),
+                pattern,
+                allPatsMatch,
+                matches = [],
+                mld,
+                i = 0;
+
+            if (patterns.length === 1) {
+                pattern = RegExp(patterns[0], 'i');
+
+                while (i < global_l) {
+                    // usersNodes[i].style.display = pattern.test(usersNodes[i].dataset.d) ? 'block' : 'none';
+
+                    if (pattern.test(global_usersArr[i].dataString)) matches.push(global_usersArrHTML[i]);
+
+                    i++;
+                }
+            } else {
+                patterns = patterns.map(pat => RegExp(pat, 'i'));
+
+                while (i < global_l) {
+                    allPatsMatch = true;
+
+                    patterns.forEach(pat => {
+                        // if (!pat.test(usersNodes[i].dataset.d)) allPatsMatch = false;
+
+                        if (!pat.test(global_usersArr[i].dataString)) allPatsMatch = false;
+                    });
+
+                    // usersNodes[i].style.display = allPatsMatch ? 'block' : 'none';
+
+                    if (allPatsMatch) matches.push(global_usersArrHTML[i]);
+
+                    i++;
+                }
+            }
+
+            mld = matches.length / 10;
+
+            /* chunk */
+            ulRender.innerHTML = matches.slice(0, mld).join('');
+
+            /* rest of it */
+            setTimeout(() => {
+                ulRender.innerHTML = matches.slice(mld).join('');
+                infoBar.clear();
+            });
+        } catch (err) {
+            console.error(err);
+        }
     }
 }
